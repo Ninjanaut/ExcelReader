33213b3fe90a5cba3bc1d36b48e6538990e000ce
Ninjanaut <lukas.prn@email.cz>
2021-09-05 23:34:01 +0200

    Add XML documentation file and update docs

--- src/ExcelReader/ExcelReaderOptions.cs
+++ src/ExcelReader/ExcelReaderOptions.cs
@@ -4,12 +4,42 @@ namespace Ninjanaut.IO
 {
     public class ExcelReaderOptions
     {
+        /// <summary>
+        /// Default format is XLSX.
+        /// </summary>
         public ExcelReaderFormat Format { get; set; }
+
+        /// <summary>
+        /// The index of the excel sheet that you want to convert to a datatable object.
+        /// Setting SheetIndex with SheetName will throw an exception.
+        /// If SheetName is not set, the default value is 0.
+        /// </summary>
         public int? SheetIndex { get; set; }
+
+        /// <summary>
+        /// The name of the excel sheet that you want to convert to a datatable object.
+        /// Setting SheetName with SheetIndex will throw an exception.
+        /// </summary>
         public string SheetName { get; set; }
-        public int? MaxColumns { get; set; } 
+
+        /// <summary>
+        /// Default value is null. I recommend setting this value so that you don't accidentally load empty columns.
+        /// </summary>
+        public int? MaxColumns { get; set; }
+
+        /// <summary>
+        /// Default value is 0.
+        /// </summary>
         public int? HeaderRowIndex { get; set; }
+
+        /// <summary>
+        /// Default value is true.
+        /// </summary>
         public bool RemoveEmptyRows { get; set; }
+
+        /// <summary>
+        /// Default value is true.
+        /// </summary>
         public bool AllowDuplicateColumns { get; set; }
 
         public ExcelReaderOptions()
@@ -18,6 +48,7 @@ namespace Ninjanaut.IO
             RemoveEmptyRows = true;
             AllowDuplicateColumns = true;
             HeaderRowIndex = 0;
+            MaxColumns = null;
         }
 
         public void Validate()
